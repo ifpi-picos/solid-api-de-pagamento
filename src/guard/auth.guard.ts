1062f61fd7344f70149941ff31c4b6427139698c
jacksonPrimo <jackson.primo12@gmail.com>
2021-09-09 23:35:58 -0300

    adjusted guard middleware

--- src/guard/auth.guard.ts
+++ src/guard/auth.guard.ts
@@ -1,10 +1,25 @@
 import { Request, Response } from "express"
-
+import jwt from 'jsonwebtoken'
+import { TokenPayloadDto } from "../dtos/tokenPayload.dto"
 export class AuthGuard {
   public logged(req: Request, resp: Response, next: Function){
     const { authorization } = req.headers
     if(!authorization){
-      throw("erro")
+      resp.status(401).send("token necessário")
+    } else {
+      const token = jwt.verify(authorization, process.env.JWT_SECRET)
+      if(!token){
+        resp.status(401).send("token expirado ou inválido")
+      } else {
+        next()
+      }
+    }
+  }
+  public admin(req: Request, resp: Response, next: Function){
+    const { authorization } = req.headers
+    const token = jwt.decode(authorization) as TokenPayloadDto
+    if(token.rule !== 'admin'){
+      resp.status(401).send('user rule is not admin')
     } else {
       next()
     }
